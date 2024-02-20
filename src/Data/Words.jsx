02230f52ebea402020ff9d9fd890c559fd6aec41
Liz Oliveira Souza Rabaçal <132154186+LizRabacal@users.noreply.github.com>
2024-02-19 21:09:11 -0300

    Update Words.jsx

--- src/Data/Words.jsx
+++ src/Data/Words.jsx
@@ -1,7 +1,11 @@
 export const Words = {
-    fruta: ["banana", "maçã", "pêra", "mamão", "laranja"],
-    cor: ["azul", "vermelho", "amarelo", "verde", "roxo"],
-    país: ["Brasil", "França", "Japão", "Austrália", "Canadá"],
-    animal: ["leão", "elefante", "girafa", "tigre", "lobo"],
-    profissão: ["médico", "engenheiro", "professor", "advogado", "chef"]
+    fruta: ["banana", "maçã", "pêra", "mamão", "laranja", "abacaxi", "uva", "morango", "melancia", "limão"],
+    cor: ["azul", "vermelho", "amarelo", "verde", "roxo", "laranja", "rosa", "preto", "branco", "marrom"],
+    país: ["Brasil", "França", "Japão", "Austrália", "Canadá", "Itália", "Índia", "Alemanha", "Espanha", "Argentina"],
+    animal: ["leão", "elefante", "girafa", "tigre", "lobo", "cachorro", "gato", "pássaro", "coelho", "macaco"],
+    profissão: ["médico", "engenheiro", "professor", "advogado", "chef", "cientista", "artista", "programador", "designer", "bombeiro"],
+    transporte: ["carro", "avião", "bicicleta", "barco", "ônibus", "trem", "helicóptero", "caminhão", "moto", "metrô"],
+    instrumento: ["violino", "piano", "guitarra", "flauta", "bateria", "saxofone", "trompete", "violoncelo", "clarinete", "acordeão"]
+    // Adicione mais temas e palavras conforme necessário
 }
+
